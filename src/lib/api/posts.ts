60ffe8f69451cb235b338bfc10aa930d2adc2fef
eunbin-lee <euns2bin@gmail.com>
2021-06-05 16:26:43 +0900

    feat: Add api settings

---
+++ src/lib/api/posts.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import BASE_URL from './api';
+
+export async function getPosts() {
+  const response = await axios.get<Post[]>(`${BASE_URL}/posts`);
+  return response.data;
+}
+
+export async function getPost(postId: number) {
+  const response = await axios.get<Post>(`${BASE_URL}/posts/${postId}`);
+  return response.data;
+}
+
+export interface Post {
+  id: number;
+  postImg: string;
+  postTitle: string;
+  postSubtitle: string;
+  postContent: string;
+  createdAt: Date;
+  likes: number;
+  userId: number;
+  userName: string;
+  userImg: string;
+}
